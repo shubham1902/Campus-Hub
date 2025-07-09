bf313df13b3a9b96391129287002d6aa15d72b38
shubham joshi <shubhamjoshi1902@gmail.com>
2025-07-09 18:03:47 +0530

    .

--- campusHUB/Backend/server.js
+++ campusHUB/Backend/server.js
@@ -8,9 +8,6 @@ const userRoutes = require("./routes/userRoutes");
 app.use(cors());
 app.use(express.json());
 app.use("/", userRoutes);
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
